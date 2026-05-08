3d7c2fe260d4b8d04679a5cbed44abf29fc7a50c
JAMES <amacollege168@gmail.com>
2026-05-09 02:43:01 +0800

    有新增及修改

--- example3_7.py
+++ example3_7.py
@@ -1,1 +1,18 @@
 import pandas as pd
+sessions=pd.read_csv('data/raw/sessions.csv')
+def lesson3(sessions)->None:
+    print('Lesson 3:profiling,missing value,dtype,datetime')
+    ## 1.查看所有欄位的型別
+    
+    print('='*30)
+    ## 2.查看 compaign 欄的前五大類別
+    
+
+    print('='*30)
+    ## 3.將 session_start 從字串轉為 datetime
+    
+
+    print('='*30)
+    ## 4.印出資料覆蓋的時間範圍
+    
+lesson3(sessions)
